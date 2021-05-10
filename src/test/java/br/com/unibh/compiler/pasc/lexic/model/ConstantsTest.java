adcaeab52f180760aa8b0a66208feb136b79bd0b
lucasbiel7 <lucasbiel7@icloud.com>
2021-05-10 20:03:28 -0300

    Teste do modelo de constantes

---
+++ src/test/java/br/com/unibh/compiler/pasc/lexic/model/ConstantsTest.java
@@ -0,0 +1,17 @@
+package br.com.unibh.compiler.pasc.lexic.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Criação de testes para validar constantes como string numero e nome de identificadores")
+class ConstantsTest {
+
+
+    @Test
+    @DisplayName("Ao pegar valor deve soltar uma exceção de operação não suportada")
+    void testeWhenGetValueOfConstantThrowsAndException() {
+        assertThrows(UnsupportedOperationException.class, () -> Constants.CHAR_CONST.getValue());
+    }
+}
