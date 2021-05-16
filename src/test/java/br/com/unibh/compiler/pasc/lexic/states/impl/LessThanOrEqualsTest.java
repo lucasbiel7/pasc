7cf7929d09cad6e99ecdd23f3a6fbaf5e887c733
Cleiton Ribeiro <cleitonsilvaribeiro@gmail.com>
2021-05-16 19:42:24 -0300

    refatorado: Testes unitários GreterThanOrEqualsTest e LessThanOrEqualsTest

--- src/test/java/br/com/unibh/compiler/pasc/lexic/states/impl/LessThanOrEqualsTest.java
+++ src/test/java/br/com/unibh/compiler/pasc/lexic/states/impl/LessThanOrEqualsTest.java
@@ -1,24 +1,18 @@
 package br.com.unibh.compiler.pasc.lexic.states.impl;
 
 import br.com.unibh.compiler.pasc.lexic.states.State;
+import br.com.unibh.compiler.pasc.lexic.states.ValidateStateHelperTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Teste unitário para verificar se o programa consegue alcançar o estado de MENOR IGUAL")
-class LessThanOrEqualsTest {
+class LessThanOrEqualsTest extends ValidateStateHelperTest {
     @Test
     @DisplayName("Verificando se valida MENOR IGUAL")
     void testWhenHasEqualsState() {
-        final char[] lessEqualWorld = "<=>".toCharArray();
-        State actualState = InitialState.getInstance();
-        int i = 0;
-        do {
-            actualState = actualState.nextState(lessEqualWorld[i]);
-            i++;
-        } while (i < lessEqualWorld.length);
-
-        assertTrue(actualState instanceof LessThanOrEqualsState);
+        State state = runProgram("<=");
+        assertTrue(state instanceof LessThanOrEqualsState);
     }
 
 }
