69d15723edb72e2d3c9a7c68c5c4d6bac373fb80
Cleiton Ribeiro <cleitonsilvaribeiro@gmail.com>
2021-05-16 20:35:01 -0300

    Teste de integração pragrama 5 'operador de diferente'

--- src/main/java/br/com/unibh/compiler/pasc/lexic/configuration/PanicModeConfig.java
+++ src/main/java/br/com/unibh/compiler/pasc/lexic/configuration/PanicModeConfig.java
@@ -2,7 +2,7 @@ package br.com.unibh.compiler.pasc.lexic.configuration;
 
 public class PanicModeConfig {
 
-    public static final int RETRY = 3;
+    public static final int RETRY = 999;
     public static final String TOKEN_ERROR_NAME = "ERRO";
 
 }
