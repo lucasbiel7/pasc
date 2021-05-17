9150ae1db93d5ec8a59afa453240fcaf4f8e9de7
lucasbiel7 <lucasbiel7@icloud.com>
2021-05-16 23:54:40 -0300

    Ajuste testes

--- src/test/java/br/com/unibh/compiler/pasc/lexic/states/impl/GreaterThanOrEqualsTest.java
+++ src/test/java/br/com/unibh/compiler/pasc/lexic/states/impl/GreaterThanOrEqualsTest.java
@@ -1,20 +1,25 @@
 package br.com.unibh.compiler.pasc.lexic.states.impl;
 
+import br.com.unibh.compiler.pasc.lexic.model.Operators;
 import br.com.unibh.compiler.pasc.lexic.states.State;
 import br.com.unibh.compiler.pasc.lexic.states.ValidateStateHelperTest;
-import lombok.experimental.Helper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Teste para identificar números MAIOR IGUAL através dos estados")
 class GreaterThanOrEqualsTest extends ValidateStateHelperTest {
     @Test
     @DisplayName("Verificando se é MAIOR IGUAL QUE")
-    void testWhenGreaterThanOrEqual(){
+    void testWhenGreaterThanOrEqual() {
         State state = runProgram(">=");
         assertTrue(state instanceof GreaterThanOrEqualsState);
+        if (state instanceof GreaterThanOrEqualsState greaterThanOrEqualsState) {
+            assertEquals(Operators.OP_GT.getValue(), greaterThanOrEqualsState.value());
+            assertEquals(Operators.OP_GT.getTokenName(), greaterThanOrEqualsState.name());
+        }
     }
 
 }
